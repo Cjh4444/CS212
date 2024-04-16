bea8dc147d751894732e2e5778c896bdaf9beeb5
Camden Harris <camdenharris@gmail.com>
2024-04-15 21:43:20 -0700

    code mostly worky except for <... it was working last night god damnit

---
+++ Program3/CalendarDate.h
@@ -0,0 +1,57 @@
+#ifndef CALENDAR_DATE
+#define CALENDAR_DATE
+
+#include <iostream>
+using namespace std;
+
+class CalendarDate {
+    
+    int month, day, year;
+
+    public:
+        CalendarDate::CalendarDate(int m, int d, int y) {
+            month = m;
+            day = d;
+            year = y;
+        }
+        string ToString() { return (to_string(month) + "/" + to_string(day) + "/" + to_string(year)); }
+        const CalendarDate& CalendarDate::operator++() {
+            ++(this->year);
+            return *this;
+        }
+
+        const CalendarDate CalendarDate::operator++(int) {
+            CalendarDate temp(*this);
+            ++(this->year);
+            return temp;
+        }
+        bool operator<(const CalendarDate& other) const {
+            if (year != other.year) return year < other.year;
+            if (month != other.month) return month < other.month;
+            return day < other.day;
+        }
+        friend ostream& operator<<(ostream& os, const CalendarDate& date) {
+            string month_string;
+            switch(date.month) {
+                case 1: month_string = "January"; break;
+                case 2: month_string = "February"; break;
+                case 3: month_string = "March"; break;
+                case 4: month_string = "April"; break;
+                case 5: month_string = "May"; break;
+                case 6: month_string = "June"; break;
+                case 7: month_string = "July"; break;
+                case 8: month_string = "August"; break;
+                case 9: month_string = "September"; break;
+                case 10: month_string = "October"; break;
+                case 11: month_string = "November"; break;
+                case 12: month_string = "December"; break;
+            }
+
+            return os << month_string << ", " << to_string(date.day) << ", " << to_string(date.year);
+        }
+
+};
+
+
+
+#endif
